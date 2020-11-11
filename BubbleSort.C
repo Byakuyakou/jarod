fa957c30cfb557a1bee5cabcd3f8c4c12897c806
jarod <ldj7902@163.com>
2020-11-11 23:53:38 +0800

    second commit

--- BubbleSort.C
+++ BubbleSort.C
@@ -1,18 +1,142 @@
-void bubbleSort(int a[], int n){
-	if (n <= 1)	return;
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-	for(int i = 0; i < n; ++i){
-		//提前退出冒泡循环的标志位
-		bool flag = false;
-		for(int j = 0; j < n - 1; ++j){
-			if(a[j] > a[j+1]){ //交换
-				int tmp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = tmp;
-				flag = true; //	表示有数据交换
+struct array{
+	int size;
+	int used;
+	int *arr;
+};
+
+void dump(struct array *array)
+{
+	int idx;
+
+	for(idx = 0; idx < array->used; idx++)
+		printf("[%02d]: %08d\n",idx, array->arr[idx]);
+}
+
+void alloc(struct array *array)
+{
+	array->arr = (int *)malloc(array->size *sizeof(int));
+}
+
+void bubble_sort(struct array *array)
+{
+	int i,j;
+
+	if(array->used <= 1)
+		return;
+
+	for(i = 0; i < array->used; i++){
+		bool has_swap = false;
+		for(j = 0; j < array->used -i -1; j++){
+			if(array->arr[j] > array->arr[j+1]){
+				int tmp;
+				tmp = array->arr[j];
+				array->arr[j] = array->arr[j+1];
+				array->arr[j+1] = tmp;
+				has_swap = true;
 			}
 		}
-		if(!flag) break; //没有数据交换，提前退出
+		if(!has_swap)
+			break;
+	}
+
+}
+
+void bubble_sort_test()
+{
+	int idx;
+	struct array ten_int = {10, 0, NULL};
+
+	alloc(&ten_int);
+	for(idx = 0; idx < 10; idx++)
+		ten_int.arr[idx] = 30 - idx;
+	ten_int.used = 10;
+	dump(&ten_int);
+	bubble_sort(&ten_int);
+	dump(&ten_int);
+}
+
+void insertion_sort(struct array *array)
+{
+	int i, j;
+
+	if(array->used <= 1)
+		return;
+
+	for(i = 1; i < array->used; i++){
+		int val = array->arr[i];
+
+			for(j = i - 1; j>= 0; j--){
+				if(val < array->arr[j])
+					array->arr[j+1] = array->arr[j];
+				else
+					break;
+			}
+			array->arr[j+1] = val;
+		}
+}
+
+void insertion_sort_test()
+{
+	int idx;
+	struct array ten_int = {10, 0, NULL};
+
+	alloc(&ten_int);
+	for(idx = 0; idx < 10; idx++)
+		ten_int.arr[idx] = 30 - idx;
+	ten_int.used = 10;
+	dump(&ten_int);
+	insertion_sort(&ten_int);
+	dump(&ten_int);
+}
+
+void selection_sort(struct array *array)
+{
+	int i, j;
+
+	if(array->used <= 1)
+		return;
+
+	for(i = 0; i < array->used-1; i++){
+		int tmp, idx = i;
+
+		for(j = 0; j < array->used; j++)
+			if(array->arr[j] < array->arr[idx])
+				idx = j;
+
+		if(idx = i)
+			continue;
+
+		tmp = array->arr[i];
+		array->arr[i] = array->arr[idx];
+		array->arr[idx] = tmp;
 	}
 }
+
+void selection_sort_test()
+{
+	int idx;
+	struct array ten_int = {10, 0, NULL};
+
+	alloc(&ten_int);
+	for(idx = 0; idx < 10; idx++)
+		ten_int.arr[idx] = 30 - idx;
+	ten_int.used = 10;
+	dump(&ten_int);
+	selection_sort(&ten_int);
+	dump(&ten_int);
+}
+
+int main()
+{
+	bubble_sort_test();
+	//selection_sort_test();
+	//insertion_sort_test();
+	return 0;
+}
+
+
 
